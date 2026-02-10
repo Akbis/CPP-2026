2415fd10c2750a92c4c55320ec5e7cfab276df7c
Akbis <keskaalbert@gmail.com>
2026-02-10 23:06:11 +0100

    Działaj w końcu

--- Minesweeper/Main.cpp
+++ Minesweeper/Main.cpp
@@ -1,1 +1,35 @@
+#define ROWS 8
+#define COLUMNS 8
+#include <iostream>
 
+// 10 mines
+void Draw()
+{
+    std::cout << "   ";
+    for (int i = 0; i < COLUMNS + 1; i++)
+    {
+
+        for (int j = 0; j < ROWS + 2; j++)
+        {
+            if (j == 0 && i > 0)
+                std::cout << i - 1;
+            else if (j == 1 && i > 0)
+                std::cout << "| ";
+
+            if (i == 0 && j > 1)
+                std::cout << (char)(j + 63) << " ";
+            // else if (i==1 && j>1)
+            //     std::cout<<"__";
+
+            else if (i > 0 && j > 1)
+                std::cout << "* ";
+        }
+        std::cout << '\n';
+    }
+}
+
+int main()
+{
+    // system("clear");
+    Draw();
+}
