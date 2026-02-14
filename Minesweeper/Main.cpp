be9cb3cbf454b48360817adb20238854b5769138
Akbis <keskaalbert@gmail.com>
2026-02-15 00:06:52 +0100

    Parsing input cd

--- Minesweeper/Main.cpp
+++ Minesweeper/Main.cpp
@@ -55,31 +55,33 @@ void theDraw(MinesBoard Board)
 }
 
 void ParseInput(std::string coords){
-    // coords.erase(std::remove(coords.begin(), coords.end(), ' '), coords.end());
+    int x, y;
+    coords.erase(std::remove(coords.begin(), coords.end(), ' '), coords.end());
     // std::cout<<coords<<'\n';
     if(coords.length()<2){
+        // std::cout<<coords<<'\n';
         std::cout << "Błędne współrzędne!\n";
         std::cout << "Podaj poprawne współrzędne pola: ";
-        std::cin >> coords;
+        std::getline(std::cin, coords);
         ParseInput(coords);
     }
     // std::vector<int> coordinates;
     // char xAxis=coords[0];
     // char yAxis;
-    int x,y;
-    // x=(int)xAxis-64;
-    x=toupper(coords.at(0))-64;
-    if(x<1 || x>COLUMNS){
-        std::cout << "Błędne współrzędne!\n";
-        std::cout << "Podaj poprawne współrzędne pola: ";
-        std::cin>>coords;
-        ParseInput(coords);
+    else{
+        // x=(int)xAxis-64;
+        x=toupper(coords.at(0))-64;
+        if(x<1 || x>COLUMNS){
+            std::cout << "Błędne współrzędne!\n";
+            std::cout << "Podaj poprawne współrzędne pola: ";
+            std::getline(std::cin, coords);
+            ParseInput(coords);
+        }
+        coords.erase(0,1);
+        y=std::stoi(coords);
+        std::cout<<"cdcd "<<y<<'\n';
+        std::cout<<x<<std::endl;
     }
-    coords.erase(0,1);
-    // y=at
-    std::cout<<coords<<'\n';
-    std::cout<<x<<std::endl;
-
 }
 
 int main()
@@ -94,7 +96,8 @@ int main()
     Board.SetBoard();
 
     std::cout<< "Podaj współrzędne pola: ";
-    std::cin>>coords;
+    std::getline(std::cin,coords);
+    // std::cin>>coords;
     ParseInput(coords);
 
     // Draw(Board);
