301df5513b2d9d22e93d6a7541c67e56f2f3d6e5
Akbis <keskaalbert@gmail.com>
2026-02-25 18:43:26 +0100

    Cursed input handling v1

--- Snake/Main.cpp
+++ Snake/Main.cpp
@@ -18,11 +18,37 @@ Direction Input(char input, SnakeBoard board){
         dir=DOWN;
         break;
     default:
-        dir=board.GetDefaultDirection();
+        dir = board.GetDefaultDirection();
         break;
 
     }
     return dir;
+}
+
+Direction NcurInput(int input,SnakeBoard board){
+    Direction dir;
+    switch (input)
+    {
+    case KEY_RIGHT:
+    case (int)'d':
+        dir = RIGHT;
+        break;
+    case KEY_UP:
+    case (int)'w':
+        dir = UP;
+        break;
+    case KEY_LEFT:
+    case (int)'a':
+        dir = LEFT;
+        break;
+    case KEY_DOWN:
+    case (int)'s':
+        dir = DOWN;
+        break;
+    default:
+        dir = board.GetDefaultDirection();
+        break;
+    }
 }
 void NcurDraw(SnakeBoard board,WINDOW* win){
     for(int i=0;i<HEIGHT;i++){
@@ -38,7 +64,8 @@ void NcurDraw(SnakeBoard board,WINDOW* win){
 
 int main(){
     system("resize -s 35 80");  // resize terminal
-    // bool gameOver=false;
+    bool gameOver=false;
+    int input;
     // system("clear");
     srand(time(0));
     SnakeBoard board;
@@ -59,22 +86,18 @@ int main(){
     initscr();
     noecho();
     curs_set(0);
+    keypad(stdscr,true);
     // printw("Hello world");
 
     WINDOW *win=newwin(22,43,5,19);
     refresh();
     box(win,0,0);
-    // for (int i = 0; i < HEIGHT; i++)
-    // {
-    //     for (int j = 0; j < WIDTH; j++)
-    //     {
-    //         mvwprintw(win, i + 1, 2 * j + 2, "%c", board.GetTile(j, i));
-    //     }
-    // }
+
     wrefresh(win);
     mvprintw(27, 25, "Score:");
     // mvprintw(27,53,"%d", score);  // use when scoring implemented
     NcurDraw(board,win);
+
 
     getch();
     endwin();
