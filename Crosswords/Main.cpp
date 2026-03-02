7df297c97786ad2241697486a80dd87957d6f7e3
Akbis <keskaalbert@gmail.com>
2026-03-02 21:25:57 +0100

    EN Crosswords completed

--- Crosswords/Main.cpp
+++ Crosswords/Main.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 #include <string>
 
-int BetterLenght(std::string word){
-    int num_code_points = 0;
-    for (char byte : word)
-    {
-        if ((byte & 0xC0) != 0x80)
-        {
-            num_code_points++;
-        }
-    }
-    return num_code_points;
-}
+// int BetterLenght(std::string word){ // Might be usefull if i tried in polish
+//     int num_code_points = 0;
+//     for (char byte : word)
+//     {
+//         if ((byte & 0xC0) != 0x80)
+//         {
+//             num_code_points++;
+//         }
+//     }
+//     return num_code_points;
+// }
 
 int main(int argc, char** argv){
     if (argc != 2)
@@ -21,29 +21,45 @@ int main(int argc, char** argv){
         std::cout<<"Błędna liczba argumentów\n";
         return 0;
     }
+
+    int number_of_results=0;
+    bool isMatching;
     std::string input = argv[1];
+    std::vector<std::string> dict,results;
 
-    for(size_t i=0;i<input.length();i++){
-        std::cout<<input.at(i)<<" "; //as expected deos not work
-
-    }
-
-    std::cout << input.length() << "\n";
-
-    std::ifstream dictionary("/usr/share/dict/polish");
-    std::vector<std::string> dict;
+    std::ifstream dictionary("/usr/share/dict/british-english");
     std::string str;
 
     while (std::getline(dictionary, str))
     {
-        if (BetterLenght(str) == BetterLenght(input)) // Zawężenie obszaru wyszukiwania do słóœ o odpowiedniej długości
+        if (str.length() == input.length() && str.at(str.length()-2) != '\'') // Zawężenie obszaru wyszukiwania do słóœ o odpowiedniej długości
         {
             dict.push_back(str);
         }
         
     }
-    // for(std::string w:dict)
-        // std::cout<<w<<" "<<BetterLenght(w)<<"\n";
-    
     dictionary.close();
+    
+    for(std::string w:dict)
+    {
+        isMatching=true;
+        for (size_t i = 0; i < input.length(); i++)
+        {   
+            if (input.at(i) != '_' && input.at(i) != w.at(i))
+            {
+                isMatching=false;
+                break;                
+            }
+        }
+        if(isMatching){
+            number_of_results++;
+            results.push_back(w);
+        }
+    }
+
+    std::cout<<number_of_results<<" results found:\n";
+    for(std::string w:results)
+        std::cout<<w<<"\n";
+
+
 }
