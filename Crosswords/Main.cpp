44973800bccf0bfbcf6ccde59183dba017303c6a
Akbis <keskaalbert@gmail.com>
2026-03-02 17:10:27 +0100

    Multibyte characters issues

--- Crosswords/Main.cpp
+++ Crosswords/Main.cpp
@@ -3,6 +3,18 @@
 #include <vector>
 #include <string>
 
+int BetterLenght(std::string word){
+    int num_code_points = 0;
+    for (char byte : word)
+    {
+        if ((byte & 0xC0) != 0x80)
+        {
+            num_code_points++;
+        }
+    }
+    return num_code_points;
+}
+
 int main(int argc, char** argv){
     if (argc != 2)
     {
@@ -10,6 +22,11 @@ int main(int argc, char** argv){
         return 0;
     }
     std::string input = argv[1];
+
+    for(size_t i=0;i<input.length();i++){
+        std::cout<<input.at(i)<<" "; //as expected deos not work
+
+    }
 
     std::cout << input.length() << "\n";
 
@@ -19,14 +36,14 @@ int main(int argc, char** argv){
 
     while (std::getline(dictionary, str))
     {
-        if (str.length() == input.length()) // Zawężenie obszaru wyszukiwania do słóœ o odpowiedniej długości
+        if (BetterLenght(str) == BetterLenght(input)) // Zawężenie obszaru wyszukiwania do słóœ o odpowiedniej długości
         {
             dict.push_back(str);
         }
         
     }
-    for(std::string w:dict)
-        std::cout<<w<<" "<<w.length()<<"\n";
+    // for(std::string w:dict)
+        // std::cout<<w<<" "<<BetterLenght(w)<<"\n";
     
     dictionary.close();
 }
