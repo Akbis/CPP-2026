b776afdae1abc14a046943b99b81aa4d79f471c4
Akbis <keskaalbert@gmail.com>
2026-03-26 01:46:39 +0100

    four words take looooong time

--- Wordle/Main.cpp
+++ Wordle/Main.cpp
@@ -8,39 +8,59 @@
 bool StrCompare(std::string a, std::string b) { return a < b; }
 bool IsRepeatingLetters(std::string a); // checks if word have any repeating letters
 bool HaveSameLetters(std::string a, std::string b); // checks if two words share any letters
-bool hasAnagram(std::string str, std::vector<std::string> dict);
-    std::vector<std::string> MatchingWords(std::string str, std::vector<std::string> dict); // finds all words in a dictionary that share no letters wit string
-int main()
-{
+bool hasAnagram(std::string str, std::vector<std::string> dict); // checks is dictionary contains any anagram of given word
+std::vector<std::string> MatchingWords(std::string str, std::vector<std::string> dict); // finds all words in a dictionary that share no letters wit string
+
+int main(){
 
     std::vector<std::string> dict,temp;
 
     std::ifstream dictionary1("wordle-La.txt");
     std::ifstream dictionary2("wordle-Ta.txt");
     std::string str;
-    // int size;
-    // bool del=false;
+
 
     auto start = std::chrono::high_resolution_clock::now();
     while (std::getline(dictionary1, str))
     {
-        if(!IsRepeatingLetters(str))
+        if(!IsRepeatingLetters(str) && !hasAnagram(str, dict))
             dict.push_back(str);
     }
     dictionary1.close();
     while (std::getline(dictionary2, str))
     {
-        if (!IsRepeatingLetters(str))
+        if (!IsRepeatingLetters(str) && !hasAnagram(str,dict))
             dict.push_back(str);
     }
     dictionary2.close();
 
     
-    // size=dict.size();
-    std::sort(dict.begin(),dict.end(),StrCompare);
+    std::sort(dict.begin(),dict.end(),StrCompare); // not really necessary, mainly for my comfort - can be deleted
     temp = dict;
-    // str=dict.at(0);
-    // for(std::string str : dict){
+
+    int size = dict.size();
+    std::vector<std::string> two_words;
+    for(int i=0; i<size-1; i++){
+        for(int j=i+1; j<size; j++){
+            if(!HaveSameLetters(dict.at(i),dict.at(j)))
+                two_words.push_back(dict.at(i)+dict.at(j));
+        }
+    }
+
+    int two_size = two_words.size();
+    std::vector<std::string> four_words;
+    for (int i = 0; i < two_size - 1; i++)
+    {
+        for (int j = i + 1; j < two_size; j++)
+        {
+            // if (!(i % (int)(two_size / 100)))
+                // std::cout <<100*i/two_size<<"%\n";
+            if (!HaveSameLetters(two_words.at(i), two_words.at(j)))
+                                 four_words.push_back(two_words.at(i) + two_words.at(j));
+        }
+    }
+        // str=dict.at(0);
+        // for(std::string str : dict){
         // for (size_t i=1;i<temp.size();i++){
         //     for (int j = 0; j < 5; j++){
         //         for(int h=0;h<5;h++){
@@ -55,31 +75,29 @@ int main()
         //             break;
         //     }
         // }
-    // }
+        // }
 
-    // for (size_t j = 0; j < 1; j++)
-    // {
-    //     temp=dict;
-    //     for (size_t i = 1; i < temp.size(); i++)
-    //     {
-    //         if(HaveSameLetters(dict.at(j),temp.at(i))){
-    //             temp.erase(temp.begin() + i);
-    //             i -= 1;
-    //         }
+        // for (size_t j = 0; j < 1; j++)
+        // {
+        //     temp=dict;
+        //     for (size_t i = 1; i < temp.size(); i++)
+        //     {
+        //         if(HaveSameLetters(dict.at(j),temp.at(i))){
+        //             temp.erase(temp.begin() + i);
+        //             i -= 1;
+        //         }
 
-    //     }
-    //     for (std::string w : temp)
-    //         std::cout << w << "\n";
+        //     }
+        //     for (std::string w : temp)
+        //         std::cout << w << "\n";
 
-    //     std::cout  << "\n";
-    // }
+        //     std::cout  << "\n";
+        // }
 
+        for (std::string w : four_words)
+            std::cout << w << "\n";
 
-
-    for (std::string w : temp)
-        std::cout << w << "\n";
-
-    std::cout << temp.size() << '\n';
+    std::cout << four_words.size() << '\n';
 
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
@@ -115,11 +133,11 @@ bool hasAnagram(std::string str, std::vector<std::string> dict){
         if (str == word)
             return true;
     }
-    return false'
-
+    return false;
 }
 
-std::vector<std::string> MatchingWords(std::string str, std::vector<std::string> dict){
+    std::vector<std::string> MatchingWords(std::string str, std::vector<std::string> dict)
+{
     for (size_t i = 0; i < dict.size(); i++)
     {
         if (HaveSameLetters(str, dict.at(i)))
